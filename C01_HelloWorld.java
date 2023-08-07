9ff0285e84ca2a529e7e51fee1b2ac5d3c394d37
AhmetCan <a.canozyurek@gmail.com>
2023-08-08 01:36:08 +0300

    my First Java day

---
+++ C01_HelloWorld.java
@@ -0,0 +1,15 @@
+package day01_programlamayagiris;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");// bu şekilde kod olmayan aciklamalar yazabiliriz.
+        /*
+        birden fazla satır yazmak istiyorsak.
+        javada data girişi yapmak istiyorsak javanın istediği kurallara uymak zorundayız.
+         */
+        System.out.println("Javada ilk kodu yazdık");
+
+        int sayi = 0;
+        System.out.println(sayi);
+    }
+}
